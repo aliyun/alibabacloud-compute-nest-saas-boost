79b68a24a30a996aa189af628aee81d53b33a824
bupt_rain <lyh376240@alibaba-inc.com>
2023-11-06 09:47:36 +0800

    docs(doc): generate guid doc web

--- boost.common/src/main/java/org/example/common/config/WebConfig.java
+++ boost.common/src/main/java/org/example/common/config/WebConfig.java
@@ -21,7 +21,9 @@ import org.example.common.handler.TokenAuthenticationInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.method.annotation.AuthenticationPrincipalArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
@@ -48,5 +50,13 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(tokenAuthenticationInterceptor)
                 .addPathPatterns("/api/**")
                 .excludePathPatterns("/api/getAuthToken");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
--- boost.front/src/pages/Welcome.tsx
+++ boost.front/src/pages/Welcome.tsx
@@ -16,7 +16,7 @@
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
 import { Card, theme } from 'antd';
-import React from 'react';
+import React, {ReactNode} from 'react';
 
 /**
  * 每个单独的卡片，为了复用样式抽成了组件
@@ -153,7 +153,7 @@ const Welcome: React.FC = () => {
           >
             <InfoCard
               index={1}
-              href="https://umijs.org/docs/introduce/introduce"
+              href="https://aliyun.github.io/alibabacloud-compute-nest-saas-boost/"
               title="了解 SaaS Boost"
               desc="SaaS Boost集成了计算巢，拥有多租户架构、持续集成和持续交付（CI/CD）能力，以及套餐管理和应用程序监控功能。这些功能加速了传统软件开发者向SaaS化转型的过程，同时降低了SaaS化转型所需的成本。用户能够轻松、顺畅地在一周内完成从传统软件服务商到SaaS软件服务商的身份转变，上云不再是难题。"
             />
