13e5c2a9eb7286f993873e74a03b224a40c9c819
mengjunwei.mjw <mengjunwei.mjw@alibaba-inc.com>
2024-04-23 15:41:29 +0800

    feat(#56195566):saas_boost_parameter_manager Modify according to comments.

--- boost.common/src/main/java/org/example/common/model/ConfigParameterQueryModel.java
+++ boost.common/src/main/java/org/example/common/model/ConfigParameterQueryModel.java
@@ -1,5 +1,6 @@
 package org.example.common.model;
 
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ConfigParameterQueryModel {
 
+    @NotNull(message = "name is mandatory for this action.")
     private String name;
-
+    @NotNull(message = "encrypted is mandatory for this action.")
     private Boolean encrypted;
 }
