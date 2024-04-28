b3ea93d56535b3ba26421b770bbbc2746b85987e
bupt_rain <lyh376240@alibaba-inc.com>
2024-04-28 09:38:28 +0800

    :ambulance: feat(retry-mechanism): Implement server error retry for key transaction operations
    Changes include:
    - Addition of a retry policy for order queries, which triggers upon receiving server errors (5xx responses), utilizing an exponential backoff strategy to manage the frequency of retries.
    - Implementation of a retry mechanism for handling failures during post-payment callback processing, ensuring successful acknowledgment of payment status even in the event of temporary server unavailability.
    - Establishment of a retry process for trade creation actions, aimed at addressing failures due to server errors by attempting the operation again with a sensible delay between attempts.

--- boost.serverless/src/main/java/org/example/Serverless.java
+++ boost.serverless/src/main/java/org/example/Serverless.java
@@ -17,11 +17,13 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling
+@EnableRetry
 public class Serverless {
     public static void main(String[] args) {
         SpringApplication.run(Serverless.class);
