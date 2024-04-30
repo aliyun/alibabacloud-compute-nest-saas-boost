78cf8538f46925c2eda968d0d6a1b27d75f1709e
mengjunwei.mjw <mengjunwei.mjw@alibaba-inc.com>
2024-04-30 13:43:42 +0800

    feat(#56195566):saas_boost_parameter_manager_front  Front-end and back-end integration testing is essentially accomplished.

--- boost.front/src/store/providerInfo/actions.tsx
+++ boost.front/src/store/providerInfo/actions.tsx
@@ -1,6 +1,7 @@
 export const SET_PROVIDER_NAME = 'SET_PROVIDER_NAME';
 export const SET_PROVIDER_OFFICIAL_LINK = 'SET_PROVIDER_OFFICIAL_LINK';
 export const SET_PROVIDER_DESCRIPTION = 'SET_PROVIDER_DESCRIPTION';
+export const SET_PROVIDER_LOGO = 'SET_PROVIDER_LOGO';
 
 export const setProviderName = (name: string) => ({
     type: SET_PROVIDER_NAME,
@@ -15,4 +16,9 @@ export const setProviderOfficialLink = (link: string) => ({
 export const setProviderDescription = (description: string) => ({
     type: SET_PROVIDER_DESCRIPTION,
     payload: description,
+});
+
+export const setProviderLogo = (logo: string) => ({
+    type: SET_PROVIDER_LOGO,
+    payload: logo,
 });
--- boost.front/src/store/providerInfo/reducer.tsx
+++ boost.front/src/store/providerInfo/reducer.tsx
@@ -2,6 +2,7 @@ import {
     SET_PROVIDER_NAME,
     SET_PROVIDER_OFFICIAL_LINK,
     SET_PROVIDER_DESCRIPTION,
+    SET_PROVIDER_LOGO,
 } from './actions';
 
 // 首先，定义State和Action的类型
@@ -9,6 +10,7 @@ interface State {
     providerName: string;
     providerOfficialLink: string;
     providerDescription: string;
+    providerLogo: string;
 }
 
 interface Action {
@@ -21,6 +23,7 @@ const initialState: State = {
     providerName: "服务商待填",
     providerOfficialLink: "服务商待填官网",
     providerDescription: "服务商待填描述信息",
+    providerLogo: "/logo.png",
 };
 
 export const providerInfoReducer = (state: State = initialState, action: Action): State => {
@@ -31,6 +34,8 @@ export const providerInfoReducer = (state: State = initialState, action: Action)
             return {...state, providerOfficialLink: action.payload};
         case SET_PROVIDER_DESCRIPTION:
             return {...state, providerDescription: action.payload};
+        case SET_PROVIDER_LOGO:
+            return {...state, providerLogo: action.payload};
         default:
             return state;
     }
