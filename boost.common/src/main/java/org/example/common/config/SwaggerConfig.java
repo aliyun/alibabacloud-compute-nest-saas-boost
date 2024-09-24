304c1bf72794bd1eff73fc0ccaf09497e8e203f2
mengjunwei.mjw <mengjunwei.mjw@alibaba-inc.com>
2024-09-24 10:30:30 +0800

    :bug:feature(#59882255):saas boost swaggerurl关闭线上访问

--- boost.common/src/main/java/org/example/common/config/SwaggerConfig.java
+++ boost.common/src/main/java/org/example/common/config/SwaggerConfig.java
@@ -14,6 +14,7 @@ import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpoints
 import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.util.StringUtils;
 import springfox.documentation.builders.ApiInfoBuilder;
